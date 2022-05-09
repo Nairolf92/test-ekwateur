951052774d2e4028cf05bdc8066cc370584961af
Florian Kelnerowski <fkelnero@bouyguestelecom.fr>
2022-05-09 12:16:48 +0200

    init 2

---
+++ src/main/java/com/example/testekwateur/TestEkwateurApplication.java
@@ -0,0 +1,13 @@
+package com.example.testekwateur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEkwateurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestEkwateurApplication.class, args);
+	}
+
+}
