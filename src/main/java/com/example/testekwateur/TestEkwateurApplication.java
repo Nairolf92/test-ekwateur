e10e67cee6c162444cfa5421098e9e159fa373a0
Florian Kelnerowski <fkelnero@bouyguestelecom.fr>
2022-05-11 18:45:22 +0200

    Beta starting getComptatibleOfferList

--- src/main/java/com/example/testekwateur/TestEkwateurApplication.java
+++ src/main/java/com/example/testekwateur/TestEkwateurApplication.java
@@ -1,6 +1,7 @@
 package com.example.testekwateur;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,13 +9,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.client.RestTemplate;
+
+import com.example.testekwateur.model.CompatibleOffer;
 import com.example.testekwateur.model.Offer;
 import com.example.testekwateur.model.PromoCode;
+import com.example.testekwateur.model.ValidOffer;
 
 @SpringBootApplication
 public class TestEkwateurApplication implements CommandLineRunner{
 	
 	public boolean isPromoCodeValid = false;
+	public Integer discountValue = 0;
+	public String endDate = null;
 	public String promoCodetoTest = null;
 	
     private static Logger LOG = LoggerFactory
@@ -41,10 +47,11 @@ public class TestEkwateurApplication implements CommandLineRunner{
     		// TODO :use try catch block and proper error handling
     		if(isPromoCodeValid) {
     			// If the promo code is valid then we can fetch the offersAPI to get the offersList to put in the json file
-        		ArrayList<Offer> offerList = new ArrayList<Offer>();
-        		offerList = getOfferList(promoCodetoTest);
-    			if(!offerList.isEmpty()) {
-    				// TODO : generate json with 
+        		ArrayList<CompatibleOffer> comptatibleOfferList = new ArrayList<CompatibleOffer>();
+        		comptatibleOfferList = getCompatibleOfferList(promoCodetoTest);
+    			if(!comptatibleOfferList.isEmpty()) {
+            		ArrayList<ValidOffer> validOfferList = new ArrayList<ValidOffer>();
+    				// TODO : generate json with validOfferList
     			} else {
     				System.out.println("Aucune offre compatible trouvée");
     			}
@@ -71,18 +78,20 @@ public class TestEkwateurApplication implements CommandLineRunner{
 		
 	}
 
-	private ArrayList<Offer> getOfferList(String promoCodetoTest) {
+	private ArrayList<CompatibleOffer> getCompatibleOfferList(String promoCodetoTest) {
 		RestTemplate restTemplate = new RestTemplate();
 		ArrayList<Offer> offerList = new ArrayList<Offer>();
+		ArrayList<CompatibleOffer> comptatibleOfferList = new ArrayList<CompatibleOffer>();
 
-		// TODO :Either use webclient or restTemplate from spring boot to fetch api
+		// Fill offerList with the offerList API
+		// TODO :Either use webclient or restTemplate from spring boot to fetch api and make it w
 		restTemplate.getForObject("https://601025826c21e10017050013.mockapi.io/ekwatest/offerList", Offer.class);
 		// TODO :Need some errors handling and to transform it into an Offer object or Offer object List
 		
 		// TODO : use stream library to collect offers from validPromoCodeList array which contains promoCodetoTest
-		offerList.stream().collect(null);
+		//comptatibleOfferList = offerList.stream().filter(null).collect(Collectors.toList());
 
-		return offerList;
+		return comptatibleOfferList;
 		
 	}
 	
@@ -93,6 +102,10 @@ public class TestEkwateurApplication implements CommandLineRunner{
             // If the promoCode exist then look if the date is still valid
             if(promoCodeinList.getCode().equals(promoCodetoTest)) {
             	if(!promoCodeinList.isDatePast(promoCodeinList.getEndDate(), "yyyy-MM-dd")) {
+            		// Need to get the discount value here because it isn't present in the offers api
+            		discountValue = promoCodeinList.getDiscountValue();
+            		// Need to get the endDate value here because it isn't present in the offers api
+            		endDate = promoCodeinList.getEndDate();
             		return true;
             	}
             }
