95a2f5b79be8035e1b448ffc15ed8d302bfcefb3
Florian Kelnerowski <fkelnero@bouyguestelecom.fr>
2022-05-11 09:52:29 +0200

    git ignore + changed files location

--- src/main/java/com/example/testekwateur/TestEkwateurApplication.java
+++ src/main/java/com/example/testekwateur/TestEkwateurApplication.java
@@ -1,10 +1,17 @@
 package com.example.testekwateur;
 
+import java.util.ArrayList;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.testekwateur.model.Offer;
 
 @SpringBootApplication
 public class TestEkwateurApplication implements CommandLineRunner{
@@ -18,13 +25,24 @@ public class TestEkwateurApplication implements CommandLineRunner{
         LOG.info("APPLICATION FINISHED");
 	}
 	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		ArrayList<Offer> offerList = new ArrayList<Offer>();
+		return args -> {
+			offerList = restTemplate.getForObject(
+					"https://601025826c21e10017050013.mockapi.io/ekwatest/offerList", Offer.class);
+			LOG.info(offerList.toString());
+		};
+	}
+	
 	@Override
 	public void run(String... args) throws Exception {
-        LOG.info("EXECUTING : command line runner");
-        
-        for (int i = 0; i < args.length; ++i) {
-            LOG.info("args[{}]: {}", i, args[i]);
-        }
+		
 	}
 
 }
